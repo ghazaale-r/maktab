9978742a63f8b4507fcf801e00e96dcecb5f2d59
Ghazaleh Ranjbaran <ranjbaran.gh@gmail.com>
2021-08-15 20:47:14 +0430

    Create person_class.py
    person class created

---
+++ person_class.py
@@ -0,0 +1,20 @@
+class PersonClass():
+  def __init__(self, name, family):
+    self.name = name
+    self.family = family
+  
+  def get_name(self):
+    return self.name
+  
+  def get_family(self)
+    return self.family
+  
+  def get_full_name(self)
+    return self.name + " " + self.family
+  
+  def add_age(self, age):
+    self.age = age
+    
+  def get_age(self):
+    return self.age
+  
