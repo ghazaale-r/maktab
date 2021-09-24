c8a69f162ba089213342fdf1196138e83a38b1ae
maryam.ahmadi241376 <maryam.ahmadi241376@gmai.com>
2021-09-24 15:05:56 +0330

    person_class is added

--- person_class.py
+++ person_class.py
@@ -1,3 +1,17 @@
+import logging
+
+name = "project"
+logger = logging.getLogger("log in")
+logging.basicConfig(level=logging.DEBUG,
+                    filename='app.log',
+                    format="'%(asctime)s - %(name)s - %(levelname)s - %(message)s' \n",
+                    datefmt='%d-%b-%y %H:%M:%S')
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('your name is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
 class PersonClass():
   def __init__(self, name, family):
     self.name = name
